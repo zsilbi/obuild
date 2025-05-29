a13593d65bb893c50aaff2abc833e07b8c68f6ae
Balázs Németh <zsilbi@zsilbi.hu>
2025-05-29 19:29:36 +0200

    chore: update warning message

--- src/builders/transform.ts
+++ src/builders/transform.ts
@@ -63,7 +63,7 @@ export async function transformDir(
       dtsOutputFile.skip = true;
 
       consola.warn(
-        `Generating a declaration file "${dtsOutputFile.path}" is currently not supported.`,
+        `Generating declaration file "${dtsOutputFile.path}" is currently not supported.`,
       );
     }
   }
