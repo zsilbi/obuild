bb05a4a2cb39718766d9ccb7a64236eda9a4a749
Balázs Németh <zsilbi@zsilbi.hu>
2025-06-06 17:38:54 +0200

    test: relative extensions

---
+++ test/e2e/transform-relative/fixture/src/modules/dynamic.ts
@@ -0,0 +1,7 @@
+export const dynamicModule: Record<string, number> = {
+  test: 1,
+  tsModule: 2,
+  jsModule: 3,
+  mjsModule: 4,
+  default: 5,
+};
---
+++ test/e2e/transform-relative/fixture/src/modules/index.ts
@@ -0,0 +1,7 @@
+export const indexModule: Record<string, number> = {
+  test: 1,
+  tsModule: 2,
+  jsModule: 3,
+  mjsModule: 4,
+  default: 5,
+};
